79120159efc52a39b7a85f7d75773297b9ce6cae
강동균 <dongkyun@flask.global>
2023-01-30 17:11:11 +0900

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ allprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
 
     group = "io.github.ehdrbs0318.lazy3j"
-    version = "1.0.0"
+    version = "1.0.1"
     java.sourceCompatibility = JavaVersion.VERSION_11
 
     repositories {
